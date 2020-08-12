7d34364dc1087f9c03b5c09bbec133be23a53690
Shubhadarshie Nanda <nandamikushubha@gmail.com>
2020-08-12 17:43:28 +0530

    mod

--- client/public/worker.js
+++ client/public/worker.js
@@ -36,3 +36,13 @@ self.addEventListener('fetch', event => {
   );
 });
 
+self.addEventListener('push', event => {
+  const data = event.data.json()
+  console.log('New notification', data)
+  const options = {
+    body: data.body,
+  }
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+})
