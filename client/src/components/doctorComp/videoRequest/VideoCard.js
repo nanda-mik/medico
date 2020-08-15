ebc903afaabd5afcd89d10ba74c057755ef45837
Saiprava <rsaiprava99@gmail.com>
2020-08-15 13:07:04 +0530

    changes in videorequest

--- client/src/components/doctorComp/videoRequest/VideoCard.js
+++ client/src/components/doctorComp/videoRequest/VideoCard.js
@@ -1,25 +1,61 @@
 import React from 'react';
 import AddToCalender from "react-add-to-calendar";
 import {Card,Button} from 'react-bootstrap';
+import TextField from '@material-ui/core/TextField';
+import Axios from 'axios';
+
 
 class VideoCard extends React.Component {
     constructor(props){
         super(props);
         this.state ={
-            user : props.user
+            user : props.user,
+            startTime : "",
+            endTime : "",
+            disabled : false
         }
     }
-    event = {
-        title: "Video Appointment Session",
-        description: `You have a Video Appointment session with a patient ${this.props.user.name}`,
-        guestEmail: `${this.props.user.email}`
+    submitHandler = (e) => {
+        e.preventDefault();
+        const options = {
+          url: `${process.env.REACT_APP_LINK}/api/doctor/confirmVideoInvitation/`,
+          method: 'POST',
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+          },
+          data: {
+            title: "Video Appointment Session",
+            description: `You have a Video Appointment session with a patient ${this.props.user.name}`,
+            guestEmail: this.props.user.email,
+            startTime: this.state.startTime,
+            endTime: this.state.endTime
+          }
+        };
+    
+        Axios(options).then((res) => {
+          console.log(res);
+    
+         
+        });
+        
+        this.setState({ disabled: true });
       };
+  
+     
     render(){
         let icon = { 'calendar-plus-o': 'left' };
         let items = [
-            
             { google: 'Google' }
          ]
+         let event = {
+            title: "Video Appointment Session",
+            description: `You have a Video Appointment session with a patient ${this.props.user.name}`,
+            guestEmail: `${this.props.user.email}`,
+            startTime: `${this.state.startTime}:00-04:00`,
+            endTime: `${this.state.endTime}:00-04:00`
+          };
+         console.log(this.state.startTime);
+         console.log(this.state.endTime);
         return(
             <div>
                 <Card style={{ width: '20rem' }}>
@@ -29,10 +65,40 @@ class VideoCard extends React.Component {
                         <Card.Text>
                             I want a video session by you.
                             My email : {this.state.user.email}
+                            <form noValidate>
+                                <TextField
+                                     id="datetime-local"
+                                     placeholder="Start time"
+                                     type="datetime-local"
+                                     label="Start-time"
+                                     InputLabelProps={{
+                                     shrink: true,
+                                    }}
+                                     onChange={(e)=>this.setState({startTime : e.target.value})}
+                                />
+                                <TextField
+                                     id="datetime-local"
+                                     label="end-time"
+                                     type="datetime-local"
+                                     placeholder="End Time"
+                                     InputLabelProps={{
+                                     shrink: true,
+                                    }}
+                                     onChange={(e)=>this.setState({endTime : e.target.value})}
+                                />
+                            </form>
                         </Card.Text>
 
-                        <AddToCalender event={this.event} buttonTemplate={icon}  title="Generate event through calenders" listItems={items} buttonLabel="Schedule it on my calendar"/>
+                        
+                        <AddToCalender event={event} buttonTemplate={icon}  title="Generate event through calenders" listItems={items} buttonLabel="Schedule it on my calendar"/>
+                        {
+                            (!this.state.disabled)?
+                            <Button onClick={this.submitHandler}>Mark as Sceduled</Button>:
+                            <Button disabled>Scheduled</Button>
+                        }
+                        
                     </Card.Body>
+                    <Card.Footer className="text-muted">Requested on {this.state.user.date}</Card.Footer>
                 </Card>
             </div>
         );
