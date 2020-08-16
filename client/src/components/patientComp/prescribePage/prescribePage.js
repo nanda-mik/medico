ca286e4d6627bb4c91d82efc66174bef7ae91bf7
Saiprava <rsaiprava99@gmail.com>
2020-08-16 08:48:47 +0530

    patient communication in prescription page

--- client/src/components/patientComp/prescribePage/prescribePage.js
+++ client/src/components/patientComp/prescribePage/prescribePage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import PrescriptionList from '../prescriptionList/prescriptionList';
-import {Button } from 'react-bootstrap';
+import {Button,Form } from 'react-bootstrap';
+import './prescribePage.css';
 
 class prescribePage extends Component {
   constructor() {
     super();
     this.state = {
       data: [],
-      event: []
+      event: [],
+      prescribe : ""
     };
   }
 
@@ -25,6 +27,13 @@ class prescribePage extends Component {
     Axios(options).then((res) => {
       this.setState({ data: res.data.arr, event: res.data.event });
     });
+  }
+  sendHandler = (e) => {
+    e.preventDefault();
+    let data = {
+      patientProblem : this.state.prescribe
+    }
+    console.log(data);
   }
   submitHandler = (e) => {
     e.preventDefault();
@@ -51,7 +60,7 @@ class prescribePage extends Component {
     console.log(data);
     console.log(this.state.event);
     return (
-      <div>
+      <div className="Prescription">
         <h3>Prescriptions</h3>
         <Button onClick={this.submitHandler}>Request for Video appointment</Button> 
         <div>
@@ -60,6 +69,36 @@ class prescribePage extends Component {
           ) : (
             <h4>No prescriptions</h4>
           )}
+        </div>
+        <div className="Patient">
+        <Form>
+          <Form.Group controlId="prescribe">
+            <Form.Label>Have some problems ? Say it to your doctor</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="3"
+              required
+              autoFocus
+              type="text"
+              value={this.state.prescribe}
+              onChange={(e) => this.setState({ prescribe: e.target.value })}
+              
+            ></Form.Control>
+          </Form.Group>
+
+          <Button
+            variant="success"
+            type="submit"
+            style={{
+              border: 'none',
+              borderRadius: '30px',
+              padding: '10px 40px',
+            }}
+            onClick = {this.sendHandler}
+          >
+            Send to your doctor
+          </Button>
+        </Form>
         </div>
       </div>
     );
